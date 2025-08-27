448b626991404d534bc42aa47220ff7b706cd162
justrawcode-jte <justrawcode.jte@gmail.com>
2025-08-27 11:56:02 -0700

    Stages transfered

--- internal/src/units/units_stage.h
+++ internal/src/units/units_stage.h
@@ -75,19 +75,42 @@ namespace Units {
 		virtual void Exit() {}
 	};
 	namespace StageManager {
+		/* Load next Stage_t
+			- Doesn't load right away
+			- Waits till the next tick to load*/
 		extern void LoadNextStage( Stage_t* stage_ptr ) noexcept;
+		/* Get a pointer to the current Stage_t */
 		UNITS_NODISCARD extern Stage_t* GetCurrentStage() noexcept;
+		/* Attempts to load the next Stage_t
+			- Does load right away if there is a next Stage_t 
+			- Only call at the very beggining of the main loop */
 		UNITS_NODISCARD extern Stage_t* AttemptGetNextStage() noexcept;
+		/* Returns true if next Stage_t has been requested */
 		UNITS_NODISCARD extern const bool& NewStage() noexcept;
+		/* Returns true if current Stage_t is paused */
 		UNITS_NODISCARD extern const bool& CurrentPaused() noexcept;
+		/* Removes next Stage_t from being loaded */
 		extern void ClearNextStage() noexcept;
 
+		/* Load next SubStage_t
+			- Doesn't load right away
+			- Waits till end of Stage_t tick to load*/
 		extern void LoadNextSubStage( SubStage_t* stage_ptr ) noexcept;
+		/* Get a pointer to the current SubStage_t */
 		UNITS_NODISCARD extern SubStage_t* GetCurrentSubStage() noexcept;
+		/* Attempts to load the next SubStage_t and pause Stage_t
+			- Does load right away if there is a next SubStage_t 
+			- Only call at the very beggining of the sub loop */
 		UNITS_NODISCARD extern SubStage_t* AttemptGetNextSubStage() noexcept;
+		/* Returns true if next SubStage_t has been requested */
 		UNITS_NODISCARD extern const bool& NewSubStage() noexcept;
+		/* Unpauses SubStage_t
+			- Doesn't unload SubStage_t in case it will be loaded again */
 		extern void EndSubStage() noexcept;
+		/* Unpauses SubStage_t
+			- Does unload SubStage_t */
 		extern void ClearCurrentSubStage() noexcept;
+		/* Removes next SubStage_t from being loaded */
 		extern void ClearNextSubStage() noexcept;
 	} // namespace StageManager
 } // namespace Core
